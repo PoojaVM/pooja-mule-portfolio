9c7a6b3066a106d4710794d103348ae45ac67552
Pooja Mule <poojavm1595@gmail.com>
2023-12-10 19:51:34 -0500

    Update About Me info

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = () => {
                 I am a Graduate student pursuing Masters in Computer Science at Stevens Institute of Technology.
                 My expected graduation date is May 2024.
                 I have over 5 years of work experience as a full stack developer.
-                I have worked on different web and mobile applications using Javascript, React, React Native, Node.js, Express.js, MongoDB, MySQL, HTML, CSS, Bootstrap, Material UI, AWS, Git, Jira, Jenkins, and Docker.
+                I have worked on different web and mobile applications using Javascript, Angular, React, React Native, Python, NodeJS, MySQL, and PostgresQL.
                 </p>
             </div>
         </div>
